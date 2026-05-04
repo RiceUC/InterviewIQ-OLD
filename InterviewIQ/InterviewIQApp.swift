bc390e088ae4d83a75bdc6e1de9da074b207ea0a
RiceUC <clarice.ciputra@gmail.com>
2026-05-04 11:51:11 +0700

    Initial Commit

---
+++ InterviewIQ/InterviewIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewIQApp.swift
+//  InterviewIQ
+//
+//  Created by Clarice Harijanto on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct InterviewIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
