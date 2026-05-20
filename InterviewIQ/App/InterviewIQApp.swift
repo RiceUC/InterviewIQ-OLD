fba99d77dfb0dd6af37741ca45fc5b50c024a065
CelloYuwa <cmazzayuwa@student.ciputra.ac.id>
2026-05-20 15:50:05 +0700

    chore(setup): add initial package-by-feature structure

---
+++ InterviewIQ/App/InterviewIQApp.swift
@@ -0,0 +1,34 @@
+//
+//  InterviewIQApp.swift
+//  InterviewIQ
+//
+//  Created by Clarice Harijanto on 04/05/26.
+//https://github.com/firebase/firebase-ios-sdk
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
+
+@main
+struct YourApp: App {
+  // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
